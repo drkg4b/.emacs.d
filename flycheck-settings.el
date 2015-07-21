880a51be60425fb183067c24f1cc96705c764607
Gabriele Bertoli <gbertoli@cern.ch>
2015-07-21 11:17:10 +0200

    flycheck-settings.el: Switch to gcc instead of clang as syntax checker for c++.
    Moved the include path to appropriate .dir-locals.el in the project
    directories.
    Moving from using clang to using gcc as syntax checker.
    Added google-cpplint and google-c-style as syntax checker to improve coding
    style.

--- flycheck-settings.el
+++ flycheck-settings.el
@@ -2,12 +2,37 @@
 ;;; Commentary:
 ;;; Code:
 
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; Perhaps a more appropriate way to set these variables would be to use
+;; M-x add-dir-local-variable RET c++-mode RET var_to_set RET path_to_include
 (add-hook 'c++-mode-hook
           (lambda ()
-	    (setq flycheck-clang-standard-library "libc++")
-	    (setq flycheck-clang-language-standard "c++1y")
-	    (setq flycheck-clang-include-path
-                           '("~/work/ATLAS/sw/MonoJetAnalysis/MiniReader_v1/trunk/MiniReader/MiniReader"))))
+	    (setq flycheck-gcc-standard-library "libc++")
+	    (setq flycheck-gcc-language-standard "c++1y")))
+
+;; Disable clang as default syntax checker for c++, this variable is buffer
+;; local by default, need to use setq-default to change the global value
+(setq-default flycheck-disabled-checkers '(c/c++-clang))
+
+;; To help respecting google C++ code style, install flycheck-google-cpplint,
+;; see https://github.com/flycheck/flycheck-google-cpplint
+(eval-after-load 'flycheck
+  '(progn
+     (require 'flycheck-google-cpplint)
+     ;; Add Google C++ Style checker.
+     ;; In default, syntax checked by Clang and Cppcheck.'
+     (flycheck-add-next-checker 'c/c++-gcc
+				'(warning . c/c++-googlelint))))
+
+(custom-set-variables
+ '(flycheck-c/c++-googlelint-executable "/usr/local/bin/cpplint.py"))
+
+;; Start google-c-style
+(require 'google-c-style)
+
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-common-hook 'google-make-newline-indent)
 
 (provide 'flycheck-settings)
 ;;; flycheck-settings.el ends here
