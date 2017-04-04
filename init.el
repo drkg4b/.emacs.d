0e72ba0431d9fbaa59740cffb5691ec9648836b5
Gabriele Bertoli <gbertoli@cern.ch>
2017-04-04 11:24:17 +0200

    init.el: Added opening of .h files in c++ mode

--- init.el
+++ init.el
@@ -158,6 +158,9 @@
                            (current-buffer) t
                            (get-buffer-create "*Astyle Errors*") t))
 
+;; Open .h file in c++ mode
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
 ;; ;required by ac-math
 ;; (require 'auto-complete-config)
 ;; (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
@@ -366,3 +369,16 @@
 (provide '.emacs)
 ;;; .emacs ends here
 (put 'dired-find-alternate-file 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
+ '(flycheck-c/c++-googlelint-executable "/usr/local/bin/cpplint.py"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
