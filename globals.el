cb3c3949550e8ef4357f9870fdb9c5f48803eb4b
Gabriele Bertoli <gbertoli@cern.ch>
2017-04-04 11:22:48 +0200

    globals.el: Added shortcuts from presentation skeleton and find file at point

--- globals.el
+++ globals.el
@@ -2,8 +2,10 @@
 ;;; Commentary:
 ;;; Code:
 
-(global-set-key "\C-cp" 'presentation-skeleton)
+(global-set-key "\C-csp" 'presentation-skeleton)
 (global-set-key "\C-csf" 'function-comment)
+(global-set-key "\C-cf" 'find-file-at-point)
+
 
 (provide 'globals)
 ;;; globals.el ends here
